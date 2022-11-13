2676276a17cc904a362026848651d93301d8e97c
sownt <thonsai@outlook.com>
2022-11-13 10:11:03 +0700

    Add RxAndroid
    Use RxAndroid and Retrofit to call api

--- app/src/main/java/com/example/dinoghost/api/DinoService.java
+++ app/src/main/java/com/example/dinoghost/api/DinoService.java
@@ -1,14 +1,29 @@
 package com.example.dinoghost.api;
 
+import com.example.dinoghost.Constants;
 import com.example.dinoghost.model.Product;
 import com.example.dinoghost.model.ProductResponse;
 
 import java.util.List;
 
+import io.reactivex.rxjava3.core.Observable;
 import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.http.GET;
 
 public interface DinoService {
+    DinoService dino = new Retrofit.Builder()
+            .baseUrl(Constants.API_ENDPOINT)
+            .addConverterFactory(GsonConverterFactory.create())
+            .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
+            .build()
+            .create(DinoService.class);
+
     @GET("/products")
     Call<ProductResponse> getProductList();
+
+    @GET("/products")
+    Observable<ProductResponse> getObservableProduct();
 }
